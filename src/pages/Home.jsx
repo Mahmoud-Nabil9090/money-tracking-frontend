a2af84c8cb47f3ef9bc151096b7328d59ecde47a
Mahmoud Nabil <mahmoudlionabel@email.com>
2026-09-02 17:09:34 +0200

    Initial frontend setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Money Tracking
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
